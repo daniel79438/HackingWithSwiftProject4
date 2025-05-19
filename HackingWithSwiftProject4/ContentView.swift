803e58d38a46844e1c850cb2c01fba7a781d0155
daniel79438 <daniel79438@gmail.com>
2025-05-19 11:15:25 +0100

    Initial Commit

---
+++ HackingWithSwiftProject4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackingWithSwiftProject4
+//
+//  Created by Daniel Harris on 19/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackingWithSwiftProject4/HackingWithSwiftProject4App.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftProject4App.swift
+//  HackingWithSwiftProject4
+//
+//  Created by Daniel Harris on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftProject4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
